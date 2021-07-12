0a8af38c8c897eb823b66119c61c9ea962552f0e
win_run <66843813+Knuschel@users.noreply.github.com>
2021-07-12 20:57:25 +0200

    QRCode

---
+++ QRCode.py
@@ -0,0 +1,10 @@
+#Generate
+import pyqrcode
+url = pyqrcode.create("https://computerbase.de", error = 'H')
+print(url.png(r"C:\Users\joshu\PycharmProjects\Baby\Data\qrcode.png", scale=(8))) # module_color=(0, 255, 0, 255), background=(0, 0, 0, 255)
+
+#Decode
+from PIL import Image
+from pyzbar.pyzbar import decode
+data = decode(Image.open(r"C:\Users\joshu\PycharmProjects\Baby\Data\qrcode.png"))
+print(data)
